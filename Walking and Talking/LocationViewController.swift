7852a474fbb237562e21e532ae502fadd863e715
kieran waugh <kieran.waugh98@gmail.com>
2019-10-31 15:07:32 +0000

    Table is now displaying sections with separate data

--- Walking and Talking/LocationViewController.swift
+++ Walking and Talking/LocationViewController.swift
@@ -10,17 +10,23 @@ import UIKit
 
 class LocationViewController: UIViewController {
 
+    
+    let sectionTitle = ["Saved", "Near You"]
+    let items = [["saved 1", "saved 2"],["near 1", "near 2"]]
+    
     let delegate = UIApplication.shared.delegate as! AppDelegate
+    
+    @IBOutlet weak var tableView: UITableView!
     
     @IBOutlet weak var locationLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        //title = "veryyyyyyyyyyyy lonnnnnnnng tilleeeeee"
-        //self.navigationController?.navigationBar.prefersLargeTitles = true
-        //adjustLargeTitleSize()
+        
         locationLabel.adjustsFontSizeToFitWidth = true
+        
+
+        tableView.contentInset = UIEdgeInsets(top: 20, left: 0, bottom: 0, right: 0)
     }
     
     
@@ -32,9 +38,11 @@ class LocationViewController: UIViewController {
                 print(err)
             } else if let placemark = originPlacemark {
                 // Do something with the placemark
-                print("placemark: \((placemark.name)!)")
-                self.locationLabel.text = (placemark.name)!
-                //self.title = "Location\((placemark.name)!)"
+                if ((placemark.name) != nil) {
+                    print("placemark: \((placemark.name)!)")
+                    self.locationLabel.text = (placemark.name)!
+                }
+                
                 
             }
         }
@@ -49,25 +57,54 @@ extension UIBarButtonItem {
     }
 }
 
-//extension UIViewController {
-//  func adjustLargeTitleSize() {
-//    guard let title = title, #available(iOS 11.0, *) else { return }
-//
-//    let maxWidth = UIScreen.main.bounds.size.width - 60
-//    var fontSize = UIFont.preferredFont(forTextStyle: .largeTitle).pointSize
-//    var width = title.size(withAttributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]).width
-//
-//    while width > maxWidth {
-//      fontSize -= 1
-//        width = title.size(withAttributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: fontSize)]).width
+extension LocationViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sectionTitle.count
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if (indexPath.section == 0) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SavedCell", for: indexPath)
+            cell.textLabel?.text = items[indexPath.section][indexPath.item]
+            return cell
+        }else{
+            let cell = tableView.dequeueReusableCell(withIdentifier: "NearCell", for: indexPath)
+            cell.textLabel?.text = items[indexPath.section][indexPath.item]
+            return cell
+        }
+    }
+    
+//    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return mobileBrand[section].brandName
 //    }
-//
-//    if var titleAttributes = navigationController?.navigationBar.largeTitleTextAttributes {
-//        titleAttributes[NSAttributedString.Key.font] = UIFont.boldSystemFont(ofSize: fontSize)
-//        navigationController?.navigationBar.largeTitleTextAttributes = titleAttributes
-//
-//    }
-//  }
-//}
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.width, height: 40))
+        //view.backgroundColor = #colorLiteral(red: 1, green: 0.3653766513, blue: 0.1507387459, alpha: 1)
+        view.backgroundColor = #colorLiteral(red: 247/255, green: 247/255, blue: 247/255, alpha: 1)
+        
+        let lbl = UILabel(frame: CGRect(x: 15, y: 0, width: view.frame.width - 15, height: 40))
+        lbl.font = UIFont.systemFont(ofSize: 20)
+        lbl.text = sectionTitle[section]
+        view.addSubview(lbl)
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    
+}
 
 
