0f3e98810a4b95fc04bb5d9e102f950d5bc1f16d
Kieran Waugh <33544563+KieranWaugh@users.noreply.github.com>
2020-06-05 20:32:53 +0100

    update app delegate to

--- Walking and Talking/AppDelegate.swift
+++ Walking and Talking/AppDelegate.swift
@@ -28,8 +28,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate, CLLocationManagerDelegate
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyDj4mKyfextSfHk-0K89rCnG5H01ydabZc")
-        GMSPlacesClient.provideAPIKey("AIzaSyDj4mKyfextSfHk-0K89rCnG5H01ydabZc")
+        GMSServices.provideAPIKey("REDACTED API KEY")
+        GMSPlacesClient.provideAPIKey("REDACTED API KEY")
         
         
         
